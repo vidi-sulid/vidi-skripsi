2e227518ac6d984fbeb3d356ceb585e7dbf9cc10
vidi-sulid <154793455+vidi-sulid@users.noreply.github.com>
2024-07-13 14:37:57 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,45 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+      build: {
+        outputDir: '../public_html', // Output directory relative to project root
+        assetDir: 'assets', // Specify asset directory within public_html
+      },
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
 });
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import vue from '@vitejs/plugin-vue';
+
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//             refresh: true,
+//         }),
+//         vue({
+//             template: {
+//                 transformAssetUrls: {
+//                     base: null,
+//                     includeAbsolute: false,
+//                 },
+//             },
+//         }),
+//     ],
+// });
